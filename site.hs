c8429dbab12c8522cd467b975b4787249922c492
Sarah Brofeldt <sbrofeldt@gmail.com>
2014-09-30 01:06:21 +0200

    Cleanup

---
+++ site.hs
@@ -0,0 +1,55 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid         (mappend)
+import           Hakyll
+import           Text.Pandoc.Options
+
+
+--------------------------------------------------------------------------------
+
+
+pandoc5Compiler = pandocCompilerWith
+                  defaultHakyllReaderOptions
+                  defaultHakyllWriterOptions { writerHtml5 = True }
+
+
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandoc5Compiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
